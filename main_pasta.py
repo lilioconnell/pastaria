215666e5c22a432c657fcd051c83f65f52572d43
lilioconnell <88407104+lilioconnell@users.noreply.github.com>
2022-08-01 10:05:26 +1200

    Sprint Four Completed
    In this sprint, I create more efficient validation functions, clean up my code and solve several crashes.

--- main_pasta.py
+++ main_pasta.py
@@ -24,15 +24,20 @@ def validate_integer(message, a, b):
             print("Invalid entry")
             continue
         if my_integer < a:
-            print()
             print("The number you have entered is too small, please try again")
-            print()
         elif my_integer > b:
-            print()
             print("The number you have entered is too large, please try again")
-            print()
         else:
             return my_integer
+
+def get_letter(option_set):
+    run = True
+    while run == True:
+        user_choice = input("What option would you like to select?:").upper()
+        for i in range(0, len(option_set)):
+            if user_choice == option_set[i][0]:
+                return user_choice
+        print("Invalid entry, please try again")
 
 
 def print_list(m):
@@ -54,33 +59,58 @@ def print_list_with_indexes(m):
         output = "{:3} : {:3} : ${:<3}".format(i + 1, m[i][0], m[i][1])
         print(output)
 
+def find_name_index(c, n):
+    i=0
+    for x in c:
+        if x[0] == n:
+            return i
+        i += 1
+    return -1
+
 
 def order_pasta(m, c):
     """Add a pizza to the order."""
     print_list_with_indexes(m)
     pasta_index = validate_integer("Choose the index number of the pasta you would like to order", 1, len(m))
+    print()
     pasta_index -= 1
-    print()
-    old_amount = 0
-    number = validate_integer("How many would would you like to add? (you can order up to 5):", 0, 5)
-    print()
-    new_amount = old_amount + number
-    output_message = "You now have {} {}s in your basket.".format(new_amount, m[pasta_index][0])
-    print(output_message)
-    temp = [m[pasta_index][0], m[pasta_index][1], new_amount]
-    c.append(temp)
+    name = m[pasta_index][0]
+    index = find_name_index(c, name)
+    if index != -1:
+        output = ("You currently have {} {} in your order, you can order up to {} more".format(c[index][2], name, 5-c[index][2]))
+        print(output)
+        old_amount = c[index][2]
+        number = validate_integer("How many more would would you like to add?:", 0, 5-c[index][2])
+        print()
+        c[index][2] = old_amount + number
+        output_message = "You now have {} {}s in your basket.".format(c[index][2], m[index][0])
+        print(output_message)
+    else:
+        print("Not yet in order")
+        old_amount = 0
+        number = validate_integer("How many would would you like to add? (you can order up to 5):", 0, 5)
+        print()
+        new_amount = old_amount + number
+        output_message = "You now have {} {}s in your basket.".format(new_amount, m[pasta_index][0])
+        print(output_message)
+        temp = [m[pasta_index][0], m[pasta_index][1], new_amount]
+        c.append(temp)
 
 
-def remove_pasta(m, c):
+def remove_pasta(c):
     """Remove a pizza to the order."""
+    if len(c) == 0:
+        print("You haven't ordered any pastas yet, please try again later")
+        return None
     print("In your current order you have:")
     review_order(c)
-    pasta_index = validate_integer("Choose the index number of the pasta you would like to remove", 1, len(c))
+    pasta_index = validate_integer("Choose the index number of the pasta you would like to remove some of", 1, len(c))
     pasta_index -= 1
     print()
-    old_amount = c[pasta_index][2]
     number = validate_integer("How many would would you like to remove? (please use numbers):", 1, c[pasta_index][2])
     c[pasta_index][2] -= number
+    if c[pasta_index][2] == 0:
+        c.pop(pasta_index)
     print()
     output_message = "In your basket you now have:"
     print(output_message)
@@ -95,7 +125,6 @@ def review_order(c):
             return None
         output = "{}: {} {}'s at ${} each".format(i + 1, c[i][2], c[i][0], c[i][1])
         print(output)
-
 
 def order_cost(c):
     """Calculate the total cost of the user's order."""
@@ -117,7 +146,7 @@ def main_menu():
     customer_order = []
 
     pasta_menu = [
-        ["Linguine Gamberi ", 23],
+        ["Linguine Gamberi", 23],
         ["Fusilli Pesto", 19],
         ["Conchilglie alla Bolognese", 22],
         ["Rigatoni alla Caponata", 21],
@@ -137,12 +166,18 @@ def main_menu():
         ["Q", "Quit"]
     ]
 
+    option_set = []
+
+    for x in my_menu:
+        option_set.append(x[0])
+
+
     run = True
     while run is True:
         print("La migliore Pastaria")
         print("." * 100)
         print_list(my_menu)
-        choice = get_string("Please select your option: ->").upper()
+        choice = get_letter(option_set)
         print("." * 100)
         if choice == "A":
             print("PASTA MENU:")
@@ -156,7 +191,7 @@ def main_menu():
             review_order(customer_order)
             print("." * 100)
         elif choice == "D":
-            remove_pasta(pasta_menu, customer_order)
+            remove_pasta(customer_order)
             print("." * 100)
         elif choice == "E":
             order_cost(customer_order)
@@ -166,7 +201,7 @@ def main_menu():
             print("." * 100)
             run = False
         else:
-            print("Input was not recognised, please try again")
+            print("An error has occured, please try again")
             print("." * 100)
 
 
